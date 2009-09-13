a7c7ebfad169c11a999fd1f99204b63e319aa145
Daniel Marchlik <marchlik@unodor.cz>
2009-09-13 16:18:35 +0200

    Menu pro projektovou podsekci
    Signed-off-by: Daniel Marchlik <marchlik@unodor.cz>

--- library/Unodor/Controller/Plugin/Menu.php
+++ library/Unodor/Controller/Plugin/Menu.php
@@ -1,15 +1,26 @@
 <?php
 class Unodor_Controller_Plugin_Menu extends Zend_Controller_Plugin_Abstract
 {
+	protected $_project;
+	
 	public function preDispatch(Zend_Controller_Request_Abstract $request)
 	{
 		//Zend_View_Helper_Navigation_HelperAbstract::setDefaultAcl($acl);
 		//Zend_View_Helper_Navigation_HelperAbstract::setDefaultRole('member');
-		Zend_Registry::set('Zend_Navigation', $this->_mybaseDefaultMenu());
-		$front = Zend_Controller_Front::getInstance();
-		$router = $front->getRouter();
 		
-		//Zend_Debug::dump($router);
+		$this->_project = $request->getParam('projekt');
+
+		switch($request->module){
+			case 'mybase' :
+				if($this->_project == 0)				
+					Zend_Registry::set('Zend_Navigation', $this->_mybaseDefaultMenu());
+				else
+					Zend_Registry::set('Zend_Navigation', $this->_mybaseProjectMenu());	
+				break;
+			default :
+				Zend_Registry::set('Zend_Navigation', $this->_DefaultMenu());
+				break;
+		}
 	}
 	
 	private function _mybaseDefaultMenu()
@@ -19,7 +30,8 @@ class Unodor_Controller_Plugin_Menu extends Zend_Controller_Plugin_Abstract
 				'label' => 'Nástěnka',    
 				'module' => 'mybase',            
 				'controller' => 'index',
-				'action' => 'index',				
+				'action' => 'index',	
+				'route' => 'mybase-default',			
 				'params' => array(
 					'account' => 'unodor'
 					)
@@ -74,6 +86,136 @@ class Unodor_Controller_Plugin_Menu extends Zend_Controller_Plugin_Abstract
 					'account' => 'unodor'
 					)
 			)															
+		));
+		
+		return $container;
+	}
+	
+	private function _mybaseProjectMenu()
+	{
+		
+		$container = new Zend_Navigation(array(
+			array(                
+				'label' => 'Přehled',    
+				'module' => 'mybase',            
+				'controller' => 'index',
+				'action' => 'index',	
+				'route' => 'mybase-projekt',			
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Milníky',                
+				'controller' => 'milestone',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Tickety',                
+				'controller' => 'ticket',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Checklist',                
+				'controller' => 'checklist',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Wiki',                
+				'controller' => 'wiki',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Diskuze',                
+				'controller' => 'discussion',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Soubory',                
+				'controller' => 'files',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),	
+			array(                
+				'label' => 'Čas',                
+				'controller' => 'time',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),
+			array(                
+				'label' => 'Kalendář',                
+				'controller' => 'calendar',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),	
+			array(                
+				'label' => 'Team',                
+				'controller' => 'people',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			),	
+			array(                
+				'label' => 'Nastavení',                
+				'controller' => 'settings',
+				'action' => 'index',
+				'module' => 'mybase',
+				'route' => 'mybase-projekt',
+				'params' => array(
+					'account' => 'unodor',
+					'projekt' => $this->_project
+					)
+			)																								
 		));
 		
 		return $container;
