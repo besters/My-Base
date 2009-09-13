700fd028170bc0e83d015885a250a82ac5d2f2a8
Daniel Marchlik <marchlik@unodor.cz>
2009-09-13 19:46:22 +0200

    Account model
    Signed-off-by: Daniel Marchlik <marchlik@unodor.cz>

---
+++ application/models/Account.php
@@ -0,0 +1,37 @@
+<?php
+class Model_Account
+{
+	/**
+	 * Ukazatel na DbTable layer
+	 * @var object
+	 */
+	private $_dbTable;
+
+	public function __construct()
+	{
+		$this->_dbTable = new Model_DbTable_Account();
+	}
+	
+	/**
+	 * Zjistuje ID uctu
+	 * 
+	 * @param string $user Ucet
+	 * @return string
+	 */
+	public function getId($account)
+	{
+		$where = array('url' => $account);
+		$id = $this->_dbTable->getRow($where, array('idaccount'));		
+		return $id['idaccount'];
+	}
+	
+	public function isValidUrl($url){
+		$where = array('url' => $url);
+		$id = $this->_dbTable->getRow($where, array('idaccount'));		
+		if(empty($id)){
+			return false;
+		}else{
+			return true;
+		}	
+	}
+}
---
+++ application/models/DbTable/Account.php
@@ -0,0 +1,13 @@
+<?php
+
+class Model_DbTable_Account extends Unodor_Db_Table
+{
+
+	protected $_name = 'account';
+
+	protected $_primary = 'idaccount';
+
+	protected $_sequence = true;
+
+
+}
