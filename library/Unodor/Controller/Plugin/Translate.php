91ed9263eae46c4978cd74c7afa0df159239aeba
Daniel Marchlik <marchlik@unodor.cz>
2009-11-12 02:05:10 +0100

    Upravy ve formatovani a komentarich

--- library/Unodor/Controller/Plugin/Translate.php
+++ library/Unodor/Controller/Plugin/Translate.php
@@ -6,56 +6,32 @@
  */
 class Unodor_Controller_Plugin_Translate extends Zend_Controller_Plugin_Abstract
 {	 
-		/**
-		 * @desc Nastavení jazyku 
-		 * @todo $language(podle cookies)
-		 * @author Filip Procházka
-		 */
-	
+	/**
+	 * @desc Nastavení jazyku 
+	 * @todo $language(podle cookies)
+	 */	
 	private  $language ; 
-		
+
    	public function preDispatch(Zend_Controller_Request_Abstract $request)
-	{    	
-	
-		
+	{    			
     	$language = 'de';
     	
-    	/**
-    	 * @desc Základní třída pro použití překladu.
-    	 * @param string $language  parametr(lang)
+    	/*
     	 * @todo upgrade Cache (později)
-    	 */
-    	
+    	 */    	
     	$translate = new Zend_Translate(
 	    					'gettext', 
 	    					LANGUAGES_PATH.'/'.$language.'.mo' ,
 	    					$language
     					); 
-
-    	/**
-    	 * @desc Cesta k logu
-    	 */
-    									
+						
     	$writer = new Zend_Log_Writer_Stream(LANGUAGES_PATH.'/log/log.phtml');
-    	
-    	/**
-    	 * @desc Nastavení vlastního formátoru "logu"
-    	 */
     	
 		$formatter = new Zend_Log_Formatter_Simple('%message%' . PHP_EOL);
 		$writer->setFormatter($formatter);
 		
-		/**
-		 * @desc identifikace předem nadefinovaných :
-		 * @param formatter 
-		 *        writer 
-		 */
 		$logger = new Zend_Log();
 		$logger->addWriter($writer);
-		
-		/**
-		 * @desc Nastavení Výstupu do logu
-		 */
 		
 		$translate->setOptions(array(
 		    'log'             => $logger,
@@ -64,29 +40,8 @@ class Unodor_Controller_Plugin_Translate extends Zend_Controller_Plugin_Abstract
 		    "$"."this->language('%locale%');",
 		    'logUntranslated' => true));
 
-		
-		/**
-		  * 
-		  * @desc Nastavení jazyků
-		  * @param string $language parametr(en,cz,de...)
-		  * 
-		  */
     	$translate->setLocale($language);
     
-    	/**
-    	 * 
-    	 * @desc Registrace Zendovské třídy : Zend_Translate
-    	 * @param class $translate parametr ( class Zend_Translate )
-    	 */
 		Zend_Registry::set('Zend_Translate' , $translate);		
-  		
-		
-		/**
-		 * 
-		 * @desc Vrací překlad 
-		 * @param string $translate  param přeložený text
-		 * 
-		 */
-         return $translate ; 
     }
 }
