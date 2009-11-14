be322ce68c7159d770d143ae471687cc98fa6e92
Daniel Marchlik <marchlik@unodor.cz>
2009-11-14 21:46:01 +0100

    Optimalizace DB dotazu

--- application/models/Project.php
+++ application/models/Project.php
@@ -25,7 +25,7 @@ class Model_Project
 		
 		$query = $this->_dbTable->select()
 					   			->from('project', array('idproject', 'idaccount', 'iduser', 'idcompany', 'name', 'description', 'img', 'status'))  
-					   			->join('user', 'project.iduser = user.iduser', array('userName' => 'name', 'userSurname' => 'surname'))       
+					   			->join('user', 'project.iduser = user.iduser', array('CONCAT_WS(" ", user.name, user.surname) as jmeno'))       
 					   			->join('company', 'project.idcompany = company.idcompany', array('companyName' => 'name'))  
 					   			->where('project.idaccount = ?', $idaccount);					   			
 					   			
@@ -37,6 +37,5 @@ class Model_Project
 		
 		return $result;
 	}
-	
 
 }
