a7fae77d86aa6e57d2a337b4930d3df863b8b176
Vaultman <marchlik@unodor.cz>
2010-01-17 17:02:29 +0100

    Opraveno - validita linku

--- application/helpers/Gravatar.php
+++ application/helpers/Gravatar.php
@@ -16,6 +16,6 @@ class Unodor_View_Helper_Gravatar extends Zend_View_Helper_Abstract
 	 */
 	public function Gravatar($email, $size = 30, $type = 'identicon')
 	{
-		return '<img src="http://www.gravatar.com/avatar/'.md5($email).'?s='.$size.'&d='.$type.'" />';
+		return '<img src="http://www.gravatar.com/avatar/'.md5($email).'?s='.$size.'&amp;d='.$type.'" alt="gravatar"/>';
 	}
 }
